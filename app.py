d7f34b235b1412f04e3780463dcd5d2449293d44
FlyingwithCaptainSoumya <get.soumya2000@gmail.com>
2022-03-06 03:30:03 +0530

    update

--- app.py
+++ app.py
@@ -88,6 +88,7 @@ def index():
     return render_template('index.html')
     
 port = os.environ.get("PORT", 5000)
+
 if __name__ == '__main__':
     app.run(debug=False, host="0.0.0.0", port=port)
 # app.config['SECRET_KEY'] = '303-960-994'
